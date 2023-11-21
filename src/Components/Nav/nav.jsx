eb085c744e34c81b37871d88144f462cb2521661
adabalasatya <satya1adabala@gmail.com>
2023-11-21 17:26:18 +0530

    update

--- src/Components/Nav/nav.jsx
+++ src/Components/Nav/nav.jsx
@@ -18,13 +18,7 @@ function Nav() {
 
   const toggleMenu = () =>{
     setMenuOpen(!menuOpen);
-    const navLinks = document.querySelector('.nav-links');
-    if (navLinks) {
-      navLinks.classList.toggle('smooth-open');
-    }
   };
-
-
 
   return (
     <Router>
