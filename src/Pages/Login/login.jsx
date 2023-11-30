1546bdf6b29ddbe51693f69f4cacacc02873fea4
adabalasatya <satya1adabala@gmail.com>
2023-11-30 15:29:37 +0530

    update

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,63 +1,82 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom';
 import './login.css';
-import Header from '../../Components/Header/header'
-import Footer from '../../Components/Footer/footer'
+import Header from '../../Components/Header/header';
+import Footer from '../../Components/Footer/footer';
 
 const Login = () => {
-  const navigate = useNavigate(); // Initialize navigate
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showWarning, setShowWarning] = useState(false); // State to handle warning message
+  const [loginSuccess, setLoginSuccess] = useState(false); // State to handle login success message
 
   const handleUsernameChange = (event) => {
     setEmail(event.target.value);
+    setShowWarning(false); // Hide warning when typing in the email field
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+    setShowWarning(false); // Hide warning when typing in the password field
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Submitted:', { email, password });
-    setEmail('');
-    setPassword('');
-    navigate('/'); // Redirect to home page after successful login
+
+    if (!email || !password) {
+      setShowWarning(true); // Show warning if either field is empty
+      return; // Stop submission if fields are empty
+    }
+
+    // Simulating a successful login
+    setLoginSuccess(true);
+    setTimeout(() => {
+      setLoginSuccess(false);
+      setEmail('');
+      setPassword('');
+      navigate('/'); // Redirect to home page after successful login
+    }, 2000); // Change the timeout value as needed
   };
 
   return (
-    
-   <>
-   <Header/>
-     
-   <div className='form-container'>
-      <h2>SignIn & Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    
-    </div>
-    <Footer/>
-   </>
+    <>
+      <Header />
+      <div className="form-container">
+        <h2>SignIn & Signup</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          {showWarning && (
+            <p className='warning-msg'>Please fill in both email and password fields.</p>
+          )}
+          {loginSuccess && (
+            <p className='login-msg'>Login successful. Redirecting...</p>
+          )}
+          {!loginSuccess && (
+            <button type="submit">Login</button>
+          )}
+        </form>
+      </div>
+      <Footer />
+    </>
   );
 };
 
